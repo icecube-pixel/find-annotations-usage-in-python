f9287311fb256bbae9d1ab9d59538cefbd789315
icecube-pixel <nithinbharadwajd@outlook.com>
2021-04-11 13:40:13 +0530

    Added changes in the script to handle failure cases

--- lib/github_utils.py
+++ lib/github_utils.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 headers = {"Accept": "application/vnd.github.v3+json"}
 
-def get_top_repositories()->Union(List[str], None):
+def get_top_repositories()->List[str]:
     """
     Gets the top repositories based on 
         language python
@@ -21,6 +21,7 @@ def get_top_repositories()->Union(List[str], None):
     if response.status_code == 200:
         data = response.json()
         github_repo_data = [repo_data['html_url'] for repo_data in data['items']]
+        logger.info(github_repo_data)
         return github_repo_data
     else:
-        return None
+        return ['None']
--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import sys
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 stdout_handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stdout_handler.setFormatter(formatter)
 logger.addHandler(stdout_handler)
 
 def main():
     logger.info('Inside Main')
     get_top_repositories()
-
 
 if __name__ == "__main__":
     main()
