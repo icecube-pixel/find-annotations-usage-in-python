42b51aced1069b0dfc68f9a82acc3cdc6e8b4d37
icecube-pixel <nithinbharadwajd@outlook.com>
2021-04-11 13:55:54 +0530

    Added code to get possible functions/variables used when using typinh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 from lib.github_utils import get_top_repositories
 import sys
+import typing
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -12,6 +13,9 @@ logger.addHandler(stdout_handler)
 def main():
     logger.info('Inside Main')
     get_top_repositories()
+    static_typing_types = dir(typing)
+    logger.info(static_typing_types)
+
 
 if __name__ == "__main__":
     main()
